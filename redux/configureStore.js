b4a2e9619032b0b398a75e4b2138cdeabc8afdea
Lesquivemeau <waterfoxyt@gmail.com>
2020-04-18 18:14:05 +0200

    🚧 Persistance first steps

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,7 +1,18 @@
 import { combineReducers } from 'redux';
-import { createStore } from 'redux'
+import { createStore } from 'redux';
 
+//Persist
+import { persistStore, persistReducer } from 'redux-persist';
+import {AsyncStorage } from 'react-native';
+
+//Reducers
 import productReducer from './reducers/ProductReducer'
+
+//Persist config
+const persistConfig = {
+    key: "root",
+    storage: AsyncStorage
+};
 
 export default createStore(combineReducers({
     products: productReducer,
