dd802ed321450235c070c1ee38453c5076743893
Lesquivemeau <waterfoxyt@gmail.com>
2020-04-18 17:42:02 +0200

    🔥 Completely remove 'react-native-circular-progress'
    replaced by 'react-native-svg-charts'

--- components/Tabs/Dashboard.js
+++ components/Tabs/Dashboard.js
@@ -2,34 +2,7 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Title, Card } from 'react-native-paper';
 import { connect } from 'react-redux';
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import NutriChart from '../NutriChart'
-import { TextPath } from 'react-native-svg';
-
-/**
-|--------------------------------------------------
-                <AnimatedCircularProgress
-                    size={200}
-                    width={20}
-                    rotation={-90}
-                    backgroundWidth={10}
-                    arcSweepAngle={360}
-                    lineCap="round"
-                    fill={this.state.fill}
-                    tintColor="#81c784"
-                    backgroundColor="#3d5875"
-                    style={styles.circularProgress}>
-                    {
-                        (fill) => (
-                            <Text style={styles.inCircleProgress}>
-                                1337.4/1486
-                                Calories
-                            </Text>
-                        )
-                    }
-                </AnimatedCircularProgress>
-|--------------------------------------------------
-*/
 
 class Dashboard extends Component {
     constructor(props) {
