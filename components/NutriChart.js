2c053f9c16e32871cda7123926018398dcaf56dd
Lesquivemeau <waterfoxyt@gmail.com>
2020-04-18 16:37:43 +0200

    Implement react-native-svg-chart, add NutriChart

---
+++ components/NutriChart.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { PieChart } from 'react-native-svg-charts'
+import { Circle, G, Line, Text as SvgText, Svg } from 'react-native-svg'
+
+class NutriChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    _ComputeRawStats(data) { //Gives carbs, proteins, fats, and  goal progress in % and returns PieChart-friendly values
+        //TODO
+    }
+
+    _toPieData(data) {
+        ndata = [];
+        data.forEach(obj => {
+            value = obj.value
+            ndata.push({
+
+                value,
+                svg: { fill: obj.color },
+                key: `pie-${obj.label}`,
+
+            });
+        });
+        return ndata;
+    }
+
+    render() {
+        const data = [
+            {
+                label: 'Carbs',
+                value: 3,
+                color: '#7ED3B2',
+            },
+            {
+                label: 'Proteins',
+                value: 5,
+                color: '#D3AE85',
+            },
+            {
+                label: 'Fats',
+                value: 2,
+                color: '#FFCC88',
+            },
+            {
+                label: null,
+                value: 5,
+                color: 'none',
+            },
+        ]
+
+        const pieData = data
+            .filter(obj => obj.value > 0)
+            .map((obj) => (
+                {
+                    value: obj.value,
+                    label: obj.label,
+                    svg: { fill: obj.color },
+                    key: `pie-${obj.label}`,
+                }))
+
+        const Labels = ({ slices }) => {
+            return slices.map((slice, index) => {
+                const { labelCentroid, pieCentroid, data } = slice;
+                return (
+                    <G key={index}>
+                        <SvgText
+                            fill={data.svg.fill}
+                            stroke="none"
+                            fontSize="20"
+                            fontWeight="bold"
+                            x={labelCentroid[0]}
+                            y={labelCentroid[1] + 5}
+                            textAnchor="middle"
+                >{data.label}</SvgText>
+                    </G>
+                )
+            })
+        }
+
+        return (
+            <PieChart
+                style={{ height: 300, padding: 0 }}
+                data={pieData}
+                innerRadius={40}
+                outerRadius={55}
+                labelRadius={100}
+                sort={(a, b) => a} //No sorting, we want the order to be the same as in the "data" array
+            >
+                <Labels />
+            </PieChart>
+        )
+    }
+}
+
+export default NutriChart;
--- components/Tabs/Dashboard.js
+++ components/Tabs/Dashboard.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { Title } from 'react-native-paper';
+import { Title, Card } from 'react-native-paper';
 import { connect } from 'react-redux';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import NutriChart from '../NutriChart'
+import { TextPath } from 'react-native-svg';
 
-class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fill: 90,
-        };
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Title style={styles.title}>Dashboard</Title>
+/**
+|--------------------------------------------------
                 <AnimatedCircularProgress
                     size={200}
                     width={20}
@@ -36,6 +28,22 @@ class Dashboard extends Component {
                         )
                     }
                 </AnimatedCircularProgress>
+|--------------------------------------------------
+*/
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fill: 90,
+        };
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Title style={styles.title}>Dashboard</Title>
+                <NutriChart />
             </View>
         );
     }
